48714fed82195f7e8611466e362945de299c3192
bigsbug <booxgitboox@gmail.com>
2022-07-18 23:07:25 +0430

    add tools encryptor and decryptor data

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -7,4 +7,8 @@ INSTALLED_APPS += [
     "cryptoreader.api.apps.ApiConfig",
 ]
 
+# encrypt key for encrypting and decrypting data
+# generated with cryptography.fernet.Fernet.generate_key()
+ENCRYPT_KEY = b"f7DwTK5K6EwmA30THAxnXgBKy_v969ItANlVGhi4C-0="
+
 ROOT_URLCONF = "config.urls.dev"
