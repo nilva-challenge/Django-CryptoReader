7a3b1f40891fb9b2be8d2173a71350bb40705075
bigsbug <booxgitboox@gmail.com>
2022-07-18 19:50:00 +0430

    interface of SDKs

---
+++ third_party_sdks/sdk.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractclassmethod
+
+
+class SDK(ABC):
+    @abstractclassmethod
+    def get_query():
+        ...
+
+    @abstractclassmethod
+    def post_query():
+        ...
+
+    @abstractclassmethod
+    def new_position(self, **kwargs):
+        ...
+
+    @abstractclassmethod
+    def get_position(self):
+        ...
