1dfe84a5141d7ebb4dbe0ef02a6774df18decc74
bigsbug <booxgitboox@gmail.com>
2022-07-18 23:04:44 +0430

    register apps

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -3,6 +3,8 @@ from config.settings.base import *
 
 INSTALLED_APPS += [
     "rest_framework",
+    "cryptoreader.accounts.apps.AccountsConfig",
+    "cryptoreader.api.apps.ApiConfig",
 ]
 
 ROOT_URLCONF = "config.urls.dev"
