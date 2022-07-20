1ec5074ddf25e7a65ad16307bdc67944dc155dee
bigsbug <booxgitboox@gmail.com>
2022-07-20 19:20:23 +0430

    register models

--- cryptoreader/accounts/admin.py
+++ cryptoreader/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import KucoinAccount, User
 
 # Register your models here.
+class KucoinAccountModelAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "type",
+        "currency",
+        "balance",
+        "available",
+        "holds",
+    ]
+
+
+admin.site.register(KucoinAccount, KucoinAccountModelAdmin)
+admin.site.register(User)
