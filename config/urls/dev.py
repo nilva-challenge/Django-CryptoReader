93988cb32f8fe35c6acd8b9e68c79a450482ff21
bigsbug <booxgitboox@gmail.com>
2022-07-20 19:20:52 +0430

    add swagger to project

--- config/urls/dev.py
+++ config/urls/dev.py
@@ -1,3 +1,27 @@
+from django.urls import include
 from config.urls.base import *
 
-urlpatterns += []
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+
+urlpatterns += [
+    path(
+        "api/v1/",
+        include(("cryptoreader.api.urls", "API/V1")),
+    ),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/v1/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+]
