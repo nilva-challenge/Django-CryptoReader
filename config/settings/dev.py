728159ffe78ea4a5ab282a6a7235a6012794fae6
bigsbug <booxgitboox@gmail.com>
2022-07-21 13:52:32 +0430

    remove authentication session

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -30,7 +30,6 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
     ],
 }
 
