eac51653deb649edd408bb571613a5485ed69cd5
atabak-hooshangi <doom_horn@yahoo.com>
2022-07-27 17:31:54 +0430

    fixed entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 python manage.py superuser
 mkdir -p supervisor
 supervisord -c supervisord.conf
