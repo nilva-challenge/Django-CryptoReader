daf309e825fc0900c629fa0fe926fb080b5eb4c6
HamidReza <hr.m.szh@gmail.com>
2022-10-07 21:25:48 +0330

    Change runner.sh

--- runner.sh
+++ runner.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+echo "Create and activated venv..."
+pip install virtualenv
+python3 -m virtualenv venv
+if [ "$OSTYPE" == "linux-gnu" ]; then
+source venv/bin/activate
+elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+.\venv\Scripts\activate.bat
+fi
+pip install --upgrade pip
+pip install -r requirements.txt
+
 echo "Makemigrations and migrate..."
 python3 manage.py makemigrations --noinput 
 python3 manage.py migrate --noinput 
@@ -5,7 +18,7 @@ python3 manage.py migrate --noinput
 echo "Collectstatic..."
 python3 manage.py collectstatic --noinput 
 
-echo "Start celery worker..."
+echo "Start celery worker in background..."
 celery -A crypto_reader worker --beat --scheduler django --loglevel=info &
 
 echo "Runserver"
